bc6c314d0da5d768d2701415d3b85b3a5a6377b8
Shashi Kumar <kumarshashi5294@gmail.com>
2021-04-24 11:49:11 +0530

    added image picker service

---
+++ lib/core/services/stacked_services.dart
@@ -0,0 +1,15 @@
+import 'package:injectable/injectable.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+@module
+abstract class RegisterExternalServices {
+  @lazySingleton
+  NavigationService get navigationService;
+
+  @lazySingleton
+  SnackbarService get snackbarService;
+  @lazySingleton
+  DialogService get dialogService;
+  @lazySingleton
+  BottomSheetService get bottomSheetService;
+}
